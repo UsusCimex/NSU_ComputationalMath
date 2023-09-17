83e6ee1da1dbfd8a3662d237e66c6ef390c50ee5
d.lanin <d.lanin@g.nsu.ru>
2023-09-17 22:16:17 +0700

    edit 2 task

--- Interpolyation/solve.py
+++ Interpolyation/solve.py
@@ -1,66 +1,92 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
+import math
+import sys
 
-def func(x):
-    return np.sqrt(x) + np.cos(x)
+# Определение основной функции
+def mainFunction(x):
+    return math.sqrt(x) + math.cos(x)
 
-def lagrange_interpolation(X, Y):
-    def interpolated_function(x):
+# Функция интерполяции Лагранжа
+def lagrangeInterpolation(X):
+    def interpolatedFunction(x):
         result = 0
         for i in range(len(X)):
-            temp = Y[i]
+            temp = mainFunction(X[i])
             for j in range(len(X)):
                 if i != j:
                     temp *= (x - X[j]) / (X[i] - X[j])
             result += temp
         return result
-    return interpolated_function
+    return interpolatedFunction
 
-print("Enter 4 param: FROM TO NUMBERNODES ERRORDOT")
-inp = input().split()
-if len(inp) == 4:
-    a = float(inp[0])
-    if a < 0:
-        sys.exit()
-    b = float(inp[1])
-    if b < a:
-        sys.exit()
+# Вывод приветствия и получение параметров от пользователя
+print("Enter 4 params: FROM TO NUMBERNODES ERRORDOT")
+params = input().split()
+if len(params) != 4:
+    print("Invalid number of parameters")
+    sys.exit()
 
-    numDotOfFunc = int((b - a) * 100)
-    X = np.linspace(a + (b-a)*0.25, b - (b-a)*0.25, numDotOfFunc) #0.25 because there are jumps at the edges of the graph
-    Y = func(X)
-    plt.plot(X, Y, label="Основная функция", linewidth=4)
+# Обработка параметров
+a = float(params[0])
+if a < 0:
+    print("Invalid parameter value: FROM")
+    sys.exit()
+b = float(params[1])
+if b < a:
+    print("Invalid parameter value: TO")
+    sys.exit()
+numNodes = int(params[2])
+if numNodes < 0:
+    print("Invalid parameter value: NUMBERNODES")
+    sys.exit()
+errorDot = float(params[3])
+if errorDot < a or errorDot > b:
+    print("Invalid parameter value: ERRORDOT")
+    sys.exit()
+plt.scatter(errorDot, mainFunction(errorDot), s=50)
 
-    numNodes = int(inp[2])
-    if numNodes < 0:
-        sys.exit()
-    errorDot = float(inp[3])
-    if errorDot < a or errorDot > b:
-        sys.exit()
-    plt.scatter(errorDot, func(errorDot), s=50)
+# Вычисление точек основной функции
+numDotOfFunc = int((b - a) * 100)
+X = []
+Y = []
+mainStep = (b - a) / numDotOfFunc
+for i in range(numDotOfFunc):
+    X.append(a + mainStep * i)
+    Y.append(mainFunction(X[i]))
+plt.plot(X, Y, label="Main Function", linewidth=4)
 
-    lagX = np.linspace(a, b, numNodes)
-    lagY = func(lagX)
-    print(lagX)
-    print(lagY)
-    li = lagrange_interpolation(lagX, lagY)
+# Создание наборов точек для интерполяции Лагранжа
+lagX = []
+step = (b - a) / numNodes
+for i in range(numNodes):
+    lagX.append(a + step * i)
+interpolatedFunc1 = lagrangeInterpolation(lagX)
 
-    lagX2 = np.linspace(a, b, numNodes * 2)
-    lagY2 = func(lagX2)
-    print(lagX2)
-    print(lagY2)
-    li2 = lagrange_interpolation(lagX2, lagY2)
+lagX2 = []
+step = (b - a) / (numNodes * 2)
+for i in range(numNodes * 2):
+    lagX2.append(a + step * i)
+interpolatedFunc2 = lagrangeInterpolation(lagX2)
 
-    Y2 = li(X)
-    plt.plot(X, Y2, label=f"Лагранж на {numNodes} точках")
-    plt.scatter(errorDot, li(errorDot), s=40)
-    Y3 = li2(X)
-    plt.plot(X, Y3, label=f"Лагранж на {numNodes * 2} точках")
-    plt.scatter(errorDot, li2(errorDot), s=40)
+# Вычисление и отображение интерполированных функций
+Y2 = []
+for i in range(numDotOfFunc):
+    Y2.append(interpolatedFunc1(a + mainStep * i))
+plt.plot(X, Y2, label=f"Lagrange Interpolation with {numNodes} Nodes")
+plt.scatter(errorDot, interpolatedFunc1(errorDot), s=40)
+Y3 = []
+for i in range(numDotOfFunc):
+    Y3.append(interpolatedFunc2(a + mainStep * i))
+plt.plot(X, Y3, label=f"Lagrange Interpolation with {numNodes * 2} Nodes")
+plt.scatter(errorDot, interpolatedFunc2(errorDot), s=40)
 
-    error = abs(func(errorDot) - li(errorDot))
-    print(f"Погрешность между основной функцией и полученной по методу Лагранжа на {numNodes} точках: {error}.")
+# Вычисление и вывод погрешности
+error = abs(interpolatedFunc1(errorDot) - mainFunction(errorDot))
+print(f"Error between the main function and the Lagrange interpolation with {numNodes} nodes: {error}.")
 
+# Вывод легенды и отображение графика
 plt.legend()
 plt.show()
