7c699d6fde2ff08c94eeb30cac3a7d6d0caf6ecd
UsusCimex <d.lanin@g.nsu.ru>
2023-12-23 20:20:30 +0700

    reload solve

--- DifferenceScheme/solve.py
+++ DifferenceScheme/solve.py
@@ -1,51 +1,58 @@
-# Исправим ошибку и сформируем правильно матрицу для solve_banded
-def implicit_scheme_corrected(u, dx, dt):
-    nx = len(u)
-    # Матрица системы A и вектор b
-    A = np.zeros((3, nx))  # Трехдиагональная матрица для solve_banded
-    b = np.zeros(nx)
-    
-    # Заполнение диагоналей матрицы A
-    A[1, :] = 1  # Главная диагональ
-    A[0, 1:] = -dt / (2 * dx)  # Верхняя диагональ
-    A[2, :-1] = -dt / (2 * dx)  # Нижняя диагональ
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.linalg import solve_banded
 
-    # Вектор правой части b
-    b[1:-1] = u[1:-1] - dt / (2 * dx) * (0.5 * u[2:]**2 - 0.5 * u[:-2]**2)
+# Параметры сетки и уравнения
+L = 5.0  # длина отрезка
+T = 1.0  # общее время
+nx = 100  # число шагов по пространству
+nt = 100  # число шагов по времени
+dx = L / nx
+dt = T / nt
+c = 1.0   # скорость волны
 
-    # Граничные условия
-    b[0] = u[0]
-    b[-1] = u[-1]
+# Функция для начального условия
+def initial_condition(x):
+    return np.piecewise(x, [x < 1, (1 <= x) & (x < 4), x >= 4],
+                        [0, lambda x: np.sin(np.pi * (x - 1) / 3), 0])
 
-    # Решаем систему линейных уравнений с помощью solve_banded
-    u_new = solve_banded((1, 1), A, b)
+# Инициализация сетки
+x = np.linspace(-1, L, nx)
+u0 = initial_condition(x)
 
+# Явная схема "Крест"
+def cross_scheme(u, c, dx, dt, nx):
+    u_new = np.copy(u)
+    for i in range(1, nx - 1):
+        u_new[i] = u[i] - c * dt / (2 * dx) * (u[i+1] - u[i-1])
     return u_new
 
-# Выполнение расчетов для всех временных слоев
-u_cross_solutions = np.zeros((nt, nx))
-u_implicit_solutions = np.zeros((nt, nx))
+# Неявная схема с центральной разностью
+def implicit_scheme(u, dx, dt):
+    nx = len(u)
+    A = np.zeros((3, nx))
+    A[0, 1:] = -dt / (2 * dx)  # верхняя диагональ
+    A[1, :] = 1                # главная диагональ
+    A[2, :-1] = dt / (2 * dx)  # нижняя диагональ
+    b = np.copy(u)
+    return solve_banded((1, 1), A, b)
 
-# Задаем начальные условия для обеих схем
-u_cross_solutions[0, :] = u0
-u_implicit_solutions[0, :] = u0
+# Расчеты
+u_cross = np.zeros((nt, nx))
+u_implicit = np.zeros((nt, nx))
+u_cross[0, :] = u0
+u_implicit[0, :] = u0
 
-# Основной цикл по времени для решения уравнений
-for t in range(1, nt):
-    # Явная схема "Крест"
-    u_cross_solutions[t, :] = explicit_cross_scheme(u_cross_solutions[t - 1, :], c, dx, dt, nx)
-    # Неявная схема с центральной разностью
-    u_implicit_solutions[t, :] = implicit_scheme_corrected(u_implicit_solutions[t - 1, :], dx, dt)
+for n in range(1, nt):
+    u_cross[n, :] = cross_scheme(u_cross[n-1, :], c, dx, dt, nx)
+    u_implicit[n, :] = implicit_scheme(u_implicit[n-1, :], dx, dt)
 
-# Визуализация результатов для последнего временного слоя
+# Визуализация
 plt.figure(figsize=(12, 6))
-
-plt.plot(x, u_cross_solutions[-1, :], label='Явная схема "Крест"', marker='o')
-plt.plot(x, u_implicit_solutions[-1, :], label='Неявная схема с центральной разностью', marker='x')
-
-plt.title('Сравнение явной и неявной схем на последнем временном слое')
+plt.plot(x, u_cross[-1, :], 'o-', label='Явная схема "Крест"')
+plt.plot(x, u_implicit[-1, :], 'x-', label='Неявная схема с центральной разностью')
+plt.title('Решение задачи переноса на последнем временном слое')
 plt.xlabel('x')
 plt.ylabel('u')
 plt.legend()
-plt.grid(True)
 plt.show()
