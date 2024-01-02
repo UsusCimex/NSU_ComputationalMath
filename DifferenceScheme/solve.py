2903cd5cac08ec47125ed5893c12da87659c3b07
UsusCimex <d.lanin@g.nsu.ru>
2024-01-02 12:54:03 +0700

    fix bugs

--- DifferenceScheme/solve.py
+++ DifferenceScheme/solve.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from scipy.linalg import solve_banded
 from matplotlib.animation import FuncAnimation
 
-left = 1.5
+left = 2.0
 right = 1.0
 # Функция для начального условия
 def initial_condition(x):
     # return np.piecewise(x, [x < 1,         (1 <= x) & (x < 4)           , x >= 4],
     #                        [0    , lambda x: np.sin(np.pi * (x - 1) / 3), 0     ])
     return np.piecewise(x, [x < 0, x >= 0],
-                           [left ,right  ])
+                           [left , right ])
 
 c = 1
 def f(u):
@@ -37,11 +37,11 @@ x = np.linspace(-1, 1, nx)
 # Явная схема "Крест"
 def cross_scheme(u_old, u, dx, dt, nx):
     u_new = np.copy(u)
+    
     dt = dx / max(u)
+    # a = c
+    a = 1
     for i in range(nx):
-        # a = c
-        a = u[i]
-        print(f"Courant number: {a * dt / dx}")
         if (i == 0):
             # u_new[i] = u_old[i] - a * dt / dx * (f(u[i+1]) - f(0))
             u_new[i] = u_old[i] - a * dt / dx * (f(u[i+1]) - f(left))
@@ -50,6 +50,7 @@ def cross_scheme(u_old, u, dx, dt, nx):
             u_new[i] = u_old[i] - a * dt / dx * (f(right) - f(u[i-1]))
         else:
             u_new[i] = u_old[i] - a * dt / dx * (f(u[i+1]) - f(u[i-1]))
+    print(f"Courant number: {a * dt / dx}")
     return u_new
 
 # Явная схема Годунова
@@ -58,13 +59,14 @@ def godunov_scheme(u, dx, dt, nx):
     dt = dx / max(u)
     for i in range(nx):
         # a = c
-        a = u[i]
-        print(f"Courant number: {a * dt / dx}")
+        # a = u[i]
+        a = 1
         if i == 0:
             # u_new[i] = u[i] - a * dt / dx * f(u[i])
             u_new[i] = u[i] + a * dt / dx * (f(left) - f(u[i]))
         else:
             u_new[i] = u[i] + a * dt / dx * (f(u[i-1]) - f(u[i]))
+    print(f"Courant number: {a * dt / dx}")
     return u_new
 
 def persecution(A, b):
@@ -92,17 +94,15 @@ def persecution(A, b):
 def implicit_scheme(u, dx, dt):
     nx = len(u)
     A = np.zeros((3, nx))
-    dt = dx / max(u)
+    dt = dx / max(u) / 2
+    # a = c
     for i in range(nx):
-        # a = c
         a = u[i]
-        print(f"Courant number: {a * dt / dx}")
         A[2, i] = -dt * a / (2 * dx)  # Нижняя диагональ
         A[1, i] = 1                   # Главная диагональ
         A[0, i] = dt * a / (2 * dx)   # Верхняя диагональ
+        print(f"Courant number: {a * dt / dx}")
     nb = np.copy(u)
-    nb[0] = left
-    nb[-1] = right
     return persecution(A, nb)
 
 # Расчеты
@@ -121,6 +121,13 @@ for n in range(1, nt):
         u_cross[n, :] = cross_scheme(u_cross[n-2, :], u_cross[n-1, :], dx, dt, nx)
     print("Implicit scheme")
     u_implicit[n, :] = implicit_scheme(u_implicit[n-1, :], dx, dt)
+    u_implicit[n, :5] = u_implicit[n-1, :5]
+    u_implicit[n, -5:] = u_implicit[n-1, -5:]
+
+# print(u_implicit[0, :])
+# print(u_implicit[1, :])
+# print(u_implicit[2, :])
+# print(u_implicit[3, :])
 
 # Визуализация на последнем временном слое
 # plt.figure(figsize=(12, 6))
@@ -136,7 +143,7 @@ fig, ax = plt.subplots()
 line1, = ax.plot(x, u_cross[0, :], 'o-', label='Явная схема "Крест"')
 line2, = ax.plot(x, u_implicit[0, :], 'x-', label='Неявная схема с центральной разностью')
 ax.set_xlim(x[0], x[-1])
-ax.set_ylim(-1, 2)
+ax.set_ylim(0, 4)
 ax.set_xlabel('x')
 ax.set_ylabel('u')
 ax.set_title('Решение задачи переноса')
